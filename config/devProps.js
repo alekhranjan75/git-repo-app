7f8ae33d46188dea9b46afe127f6536d8be915d8
alekhranjan75 <alekhranjan75@gmail.com>
2020-05-20 21:34:02 +0530

    Adding development-keys

---
+++ config/devProps.js
@@ -0,0 +1,6 @@
+module.exports = {
+    gitHubClientID: '094a67388e1b09482874',
+    gitHubClientSecret: '67d17c5ec40d150da333009066ef35b8fc4326bb',
+    mongoURI: 'mongodb+srv://admin:admin@cluster0-nssvj.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'csakfnioadacsuieioIIndankwioIiOInkqwOiohqhehh',
+};
